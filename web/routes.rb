98233baa8faee5afc97be8c6a20a8059c861e423
sonots <sonots@gmail.com>
2013-04-18 03:04:39 +0900

    remove favicon.ico

--- web/routes.rb
+++ web/routes.rb
@@ -2,6 +2,9 @@
 
 module Web
   ROUTES = {
+    # Web browsers will GET the favicon.ico resource with every single request.
+    # Without a dedicated route for it:
+    '/favicon.ico' => proc { |env| [200, {'Content-Type' => 'plain/text'}, ['']] },
     '/'               => Web::FrontController,
     '/web'            => Web::FrontController,
     '/web/notifier'   => Web::NotifierController,
